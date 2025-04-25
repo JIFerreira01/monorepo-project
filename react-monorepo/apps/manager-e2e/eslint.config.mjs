2cfde0c4626528ce646faacf431df6b2aaa2fd26
Jeferson <jeferson.inacio.2@gmail.com>
2025-04-25 19:11:14 -0300

    criando outro projeto no workspace

--- react-monorepo/apps/manager-e2e/eslint.config.mjs
+++ react-monorepo/apps/manager-e2e/eslint.config.mjs
@@ -1,10 +1,17 @@
+import playwright from 'eslint-plugin-playwright';
 import cypress from 'eslint-plugin-cypress/flat';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
+  playwright.configs['flat/recommended'],
   cypress.configs['recommended'],
   ...baseConfig,
   {
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
     // Override or add rules here
     rules: {},
   },
